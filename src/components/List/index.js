9e8d05edc976191d9eca786bc4e72ca87db1e89c
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-08-16 10:36:47 -0300

    Aplicaçao pronta

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {MdAdd} from 'react-icons/md'
+import { MdAdd } from 'react-icons/md';
 import { Container } from './styles';
 import Card from '../Card';
 
-export default function List({data}) {
+export default function List({ data, index: listIndex }) {
   return (
     <Container done={data.done}>
       <header>
         <h2>{data.title}</h2>
-          {data.creatable &&
-        <button type="button">
-          <MdAdd size={24} color="#fff"/>
-        </button>
-          }
+        {data.creatable && (
+          <button type="button">
+            <MdAdd size={24} color="#fff" />
+          </button>
+        )}
       </header>
 
       <ul>
-    {data.cards.map(card => <Card key={card.id} data={card}/>)}
+        {data.cards.map((card, index) => (
+          <Card key={card.id} index={index} data={card} listIndex={listIndex} />
+        ))}
       </ul>
     </Container>
   );
